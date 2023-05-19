530fd63a5f2a77319e980fdcdc69fe71aa80b9cb
Kei Yamamoto <withwhom0215@gmail.com>
2023-05-19 13:50:42 +0200

    clean up create_visualizations.py

--- postprocessing_h5py/create_visualizations.py
+++ postprocessing_h5py/create_visualizations.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 """
-This script takes the visualization files from TurtleFSI and outputs: 
+This script takes the visualization files from turtleFSI and outputs: 
 (1) Domain-specific visualizations (wall displacement for the wall only, and velocity and pressure for the fluid only).
-    This way the simualtion can be postprocessed as a CFD simulation and a Solid Mechanics Element simulation separately. 
+    This way the simualtion can be postprocessed as a CFD simulation and a solid Mechanics simulation separately. 
 (2) Reduced save-deg visualization if save_deg = 2 (Creates a lightweight file for faster postprocessing)
 (3) Band-Pass filtered visualizations for d, v, p and strain
 
@@ -34,18 +34,8 @@ Example: --dt 0.000679285714286 --mesh file_case16_el06 --end_t 0.951
 
 def create_visualizations(case_path, mesh_name, save_deg, stride, ts, start_t, end_t, dvp, bands, perform_hi_pass):
     print(f"perform_hi_pass = {perform_hi_pass}")
+    print("")
 
-    case_name = os.path.basename(os.path.normpath(case_path)) # obtains only last folder in case_path
-    # visualization_path = postprocessing_common_h5py.get_visualization_path(case_path)
-    visualization_path = case_path + "/Visualization/"
-    bands_list = bands.split(",")
-    num_bands = int(len(bands_list)/2)
-    lower_freq = np.zeros(num_bands)
-    higher_freq = np.zeros(num_bands)
-    for i in range(num_bands):
-        lower_freq[i] = float(bands_list[2*i])
-        higher_freq[i] = float(bands_list[2*i+1])
-    
     # Get Mesh
     if save_deg == 1:
         mesh_path = case_path + "/mesh/" + mesh_name +".h5" # Mesh path. Points to the corner-node input mesh
@@ -54,48 +44,68 @@ def create_visualizations(case_path, mesh_name, save_deg, stride, ts, start_t, e
     mesh_path_sd1 = case_path + "/mesh/" + mesh_name +".h5" # Mesh path. Points to the corner-node input mesh
     mesh_path_fluid_sd1 = mesh_path_sd1.replace(".h5","_fluid_only.h5") # needed for mps
     mesh_path_solid_sd1 = mesh_path_sd1.replace(".h5","_solid_only.h5") # needed for mps
-    
-    # Get Command Line Arguments and Input File Paths
+
+    # create path for output folders
+    case_name = os.path.basename(os.path.normpath(case_path)) # obtains only last folder in case_path
+    visualization_path = os.path.join(case_path, "Visualization")
     formatted_data_folder_name = "res_"+case_name+'_stride_'+str(stride)+"t"+str(start_t)+"_to_"+str(end_t)+"save_deg_"+str(save_deg)
     formatted_data_folder = os.path.join(case_path,formatted_data_folder_name)
     visualization_separate_domain_folder = os.path.join(case_path, "Visualization_separate_domain")
-    visualization_hi_pass_folder = os.path.join(case,_path, "Visualization_hi_pass")
-    visualization_sd1_folder = os.path.join(case_path, "Visualization_sd1")
+    visualization_hi_pass_folder = os.path.join(case_path, "Visualization_hi_pass")
+    # NOTE: comment out for now as we do not need reduce_save_deg_viz 18/05/2023 Kei 
+    # visualization_sd1_folder = os.path.join(case_path, "Visualization_sd1")
     
-    #visualization_sd1_folder = os.path.join(visualization_path,"../visualization_save_deg_1")
-    
-    # For pressure, displacement or velocity
-    
-    # Create output folder and filenames
+    # Create output folder and filenames for compressed data
     output_file_name = case_name+"_"+ dvp+"_mag.npz" 
-    formatted_data_path = formatted_data_folder+"/"+output_file_name
+    formatted_data_path = os.path.join(formatted_data_folder, output_file_name)
     
     # If the output file exists, don't re-make it
     if os.path.exists(formatted_data_path):
-       print('path found!')
-    if dvp == "wss":
-        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_separate_domain_folder, formatted_data_folder, mesh_path_fluid_sd1, case_name, start_t,end_t,dvp,stride)
+       print(f'Path: {formatted_data_path} found! Will not re-make npz files')
+       time_between_input_files = postprocessing_common_h5py.get_time_between_files(visualization_path, dvp)
+
+    elif dvp == "wss":
+        print("Making compressed data:  Start time: {}, end time: {}".format(start_t,end_t))
+        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_separate_domain_folder, formatted_data_folder, mesh_path_fluid_sd1, case_name, start_t,end_t, dvp, stride)
+
     elif dvp == "mps" or dvp == "strain":
-        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_separate_domain_folder, formatted_data_folder, mesh_path_solid_sd1, case_name, start_t,end_t,dvp,stride)
+        print("Making compressed data:  Start time: {}, end time: {}".format(start_t,end_t))
+        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_separate_domain_folder, formatted_data_folder, mesh_path_solid_sd1, case_name, start_t,end_t, dvp, stride)
+
     else: 
         # Make the output h5 files with dvp magnitudes
-        print("Start time: {}, end time: {}".format(start_t,end_t))
-        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_path, formatted_data_folder, mesh_path, case_name, start_t,end_t,dvp,stride)
+        print("Making compressed data:  Start time: {}, end time: {}".format(start_t,end_t))
+        time_between_input_files = postprocessing_common_h5py.create_transformed_matrix(visualization_path, formatted_data_folder, mesh_path, case_name, start_t, end_t, dvp, stride)
     
     # Get the desired time between output files (reduce output frequency by "stride")
     time_between_output_files = time_between_input_files*stride 
+
+    # create bands for hi-pass filter
+    bands_list = bands.split(",")
+    num_bands = int(len(bands_list)/2)
+    lower_freq = np.zeros(num_bands)
+    higher_freq = np.zeros(num_bands)
+    for i in range(num_bands):
+        lower_freq[i] = float(bands_list[2*i])
+        higher_freq[i] = float(bands_list[2*i+1])
+
     if dvp != "wss" and dvp != "mps" and dvp != "strain":
+        # Create the visualization files
+        print("===Creating domain specific visualization files===")
+        print("")
         postprocessing_common_h5py.create_domain_specific_viz(formatted_data_folder, visualization_separate_domain_folder, mesh_path,save_deg, time_between_output_files,start_t,dvp)
+
         # TODO: here mesh_path should be the mesh_path_sd1 ?
         # NOTE: For now, reduce_save_deg_viz is not necessary and skip it 18/05/2023 Kei 
         # postprocessing_common_h5py.reduce_save_deg_viz(formatted_data_folder, visualization_sd1_folder, mesh_path,1, time_between_output_files,start_t,dvp)
-    # #NOTE: I do not know why save_deg needs to be 1 here. It could be 2 as well? Also, do we need high pass viz for velocity and pressure?
+
+        #NOTE: I do not know why save_deg needs to be 1 here. It could be 2 as well? Also, do we need high pass viz for velocity and pressure?
         if dvp == "d" and perform_hi_pass:
-            print("creating hi pass viz")
+            print("===creating hi pass viz===")
+            print("")
             for i in range(len(lower_freq)):
                 postprocessing_common_h5py.create_hi_pass_viz(formatted_data_folder, visualization_hi_pass_folder, mesh_path,time_between_output_files,start_t,dvp,lower_freq[i],higher_freq[i])
                 postprocessing_common_h5py.create_hi_pass_viz(formatted_data_folder, visualization_hi_pass_folder, mesh_path,time_between_output_files,start_t,dvp,lower_freq[i],higher_freq[i],amplitude=True)
-    
     
     elif dvp == "strain":
         for i in range(len(lower_freq)):
