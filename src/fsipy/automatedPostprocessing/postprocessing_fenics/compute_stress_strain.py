dba7bf1af1238708e2db9c105ec691eca68cb285
Kei <withwhom0215@gmail.com>
2023-12-01 17:04:30 +0100

    update compute_stress_strain code

--- src/fsipy/automatedPostprocessing/postprocessing_fenics/compute_stress_strain.py
+++ src/fsipy/automatedPostprocessing/postprocessing_fenics/compute_stress_strain.py
@@ -1,14 +1,27 @@
 from pathlib import Path
-
+import argparse
 from dolfin import *
 from turtleFSI.modules import common
-import os
 
-from postprocessing_common import read_command_line
+from vampy.automatedPostprocessing.postprocessing_common import get_dataset_names
 
 # set compiler arguments
-parameters["form_compiler"]["quadrature_degree"] = 6
 parameters["reorder_dofs_serial"] = False
+
+
+def parse_arguments():
+    """Read arguments from commandline"""
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--folder', type=Path, help="Path to simulation results folder")
+    parser.add_argument('--mesh-path', type=Path, default=None,
+                        help="Path to the mesh file. If not given (None), " +
+                             "it will assume that mesh is located <folder_path>/Mesh/mesh.h5)")
+    parser.add_argument("--stride", type=int, default=1, help="Save frequency of output data")
+    args = parser.parse_args()
+
+    return args
+
 
 
 def compute_stress(visualization_separate_domain_folder, mesh_path, stride):
@@ -33,13 +46,13 @@ def compute_stress(visualization_separate_domain_folder, mesh_path, stride):
     """
     try:
         file_path_d = visualization_separate_domain_folder / "d_solid.h5"
-        assert file_path_d.exists()
+        assert file_path_d.exists(), f"Displacement file {file_path_d} not found."
         solid_only = True
         if MPI.rank(MPI.comm_world) == 0:
             print("--- Using d_solid.h5 file \n")
     except AssertionError:
         file_path_d = visualization_separate_domain_folder / "d.h5"
-        assert file_path_d.exists()
+        assert file_path_d.exists(), f"Displacement file {file_path_d} not found."
         solid_only = False
         if MPI.rank(MPI.comm_world) == 0:
             print("--- displacement is for the entire domain \n")
@@ -62,111 +75,91 @@ def compute_stress(visualization_separate_domain_folder, mesh_path, stride):
     refined_mesh = Mesh()
     with HDF5File(MPI.comm_world, str(refined_mesh_path), "r") as mesh_file:
         mesh_file.read(refined_mesh, "mesh", False)
-
+    from IPython import embed; embed(); exit(1)
     # Define functionspaces and functions
     if MPI.rank(MPI.comm_world) == 0:
         print("--- Define function spaces \n")
 
-
-    # _, domains, _ = get_mesh_domain_and_boundaries(mesh_path)
-
-    print_MPI("Define function spaces and functions")
-
     # Create function space for the displacement on the refined mesh with P1 elements
     Vv_refined = VectorFunctionSpace(refined_mesh, "CG", 1)
+    d_p1 = Function(Vv_refined)
     # Create function space for the displacement on the refined mesh with P2 elements
     Vv_non_refined = VectorFunctionSpace(mesh, "CG", 2)
-
-    # Create visualization function space for d, v and p
-    dve_viz = VectorElement('CG', mesh_viz.ufl_cell(), 1)
-    FSdv_viz = FunctionSpace(mesh_viz, dve_viz)   # Visualisation FunctionSpace for d and v
-
-    # Create higher-order function on unrefined mesh for post-processing calculations
-    d = Function(FSdv)
-
-    # Create lower-order function for visualization on refined mesh
-    d_viz = Function(FSdv_viz)
-    
+    d_p2 = Function(Vv_non_refined)
+        
     # Create a transfer matrix between higher degree and lower degree (visualization) function spaces
-    dv_trans = PETScDMCollection.create_transfer_matrix(FSdv_viz,FSdv)
+    d_transfer_matrix = PETScDMCollection.create_transfer_matrix(Vv_refined, Vv_non_refined)
 
+    # NOTE: This is something I have to think about how to implement in new code
     # Set up dx (dx_s for solid, dx_f for fluid) for each domain
-    dx = Measure("dx", subdomain_data=domains)
-    dx_s = {}
-    dx_s_id_list = []
-    for idx, solid_region in enumerate(solid_properties):
-        dx_s_id = solid_region["dx_s_id"]
-        dx_s[idx] = dx(dx_s_id, subdomain_data=domains) # Create dx_s for each solid region
-        dx_s_id_list.append(dx_s_id)
-        print_MPI(solid_region)
+    # dx = Measure("dx", subdomain_data=domains)
+    # dx_s = {}
+    # dx_s_id_list = []
+    # for idx, solid_region in enumerate(solid_properties):
+    #     dx_s_id = solid_region["dx_s_id"]
+    #     dx_s[idx] = dx(dx_s_id, subdomain_data=domains) # Create dx_s for each solid region
+    #     dx_s_id_list.append(dx_s_id)
+    #     print_MPI(solid_region)
+    
+    # NOTE: For now assume there is no fluid region
+    # dx_f = {}
+    # dx_f_id_list = []
+    # for idx, fluid_region in enumerate(fluid_properties):
+    #     dx_f_id = fluid_region["dx_f_id"]
+    #     dx_f[idx] = dx(dx_f_id, subdomain_data=domains) # Create dx_s for each solid region
+    #     dx_f_id_list.append(dx_f_id)
+    #     print_MPI(fluid_region)
 
-    dx_f = {}
-    dx_f_id_list = []
-    for idx, fluid_region in enumerate(fluid_properties):
-        dx_f_id = fluid_region["dx_f_id"]
-        dx_f[idx] = dx(dx_f_id, subdomain_data=domains) # Create dx_s for each solid region
-        dx_f_id_list.append(dx_f_id)
-        print_MPI(fluid_region)
 
+    # Create function space for stress and strain
+    VT = TensorFunctionSpace(mesh, "DG", 1)
+    V = FunctionSpace(mesh, "DG", 1)
 
-    '''
-    Strain/stress are in L2, therefore we use a discontinuous function space with a degree of 1 for P2P1 elements
-    Could also use a degree = 0 to get a constant-stress representation in each element
-    For more info see the Fenics Book (P62, or P514-515), or
-    https://comet-fenics.readthedocs.io/en/latest/demo/viscoelasticity/linear_viscoelasticity.html?highlight=DG#A-mixed-approach
-    https://fenicsproject.org/qa/10363/what-is-the-most-accurate-way-to-recover-the-stress-tensor/
-    https://fenicsproject.discourse.group/t/why-use-dg-space-to-project-stress-strain/3768
-    '''
-    Te = TensorElement("DG", mesh.ufl_cell(), save_deg-1) 
-    Tens = FunctionSpace(mesh, Te)
-    Fe = FiniteElement("DG", mesh.ufl_cell(), save_deg-1) 
-    Scal = FunctionSpace(mesh, Fe)
+    # Create function space for stress and strain
+    TS = Function(VT)
+    GLS = Function(VT)
+    MPStress = Function(Scal)
+    MPStrain = Function(Scal)
 
     # NOTE: I guess sig is sigma and ep is epsilon, sig_P is sigma principal and ep_P is epsilon principal
-    sig_path = (visualization_separate_domain_path / "TrueStress.xdmf").__str__()
-    ep_path = (visualization_separate_domain_path / "InfinitesimalStrain.xdmf").__str__()
-    sig_P_path = (visualization_separate_domain_path / "MaxPrincipalStress.xdmf").__str__()
-    ep_P_path = (visualization_separate_domain_path / "MaxPrincipalStrain.xdmf").__str__()
+      # Create XDMF files for saving indices
+    stress_strain_path = visualization_separate_domain_folder.parent / "StressStrain"
+    stress_strain_path.mkdir(parents=True, exist_ok=True)
+    stress_strain_names = ["TrueStress", "Green-Lagrange-strain", "MaxPrincipalStress", "MaxPrincipalStrain"]
+    stress_strain_variables = [TS, GLS, MPStress, MPStrain]
+    stress_strain_dict = dict(zip(stress_strain_names, stress_strain_variables))
+    xdmf_paths = [hemodynamic_indices_path / f"{name}.xdmf" for name in stress_strain_names]
+
+    stress_strain = {}
+    for index, path in zip(stress_strain_variables, xdmf_paths):
+        stress_strain[index] = XDMFFile(MPI.comm_world, str(path))
+        stress_strain[index].parameters["rewrite_function_mesh"] = False
+        stress_strain[index].parameters["flush_output"] = True
+        stress_strain[index].parameters["functions_share_mesh"] = True
     
-    sig_file = XDMFFile(MPI.comm_world, sig_path)
-    ep_file = XDMFFile(MPI.comm_world, ep_path)
-    sig_P_file = XDMFFile(MPI.comm_world, sig_P_path)
-    ep_P_file = XDMFFile(MPI.comm_world, ep_P_path)
+    if MPI.rank(MPI.comm_world) == 0:
+        print("=" * 10, "Start post processing", "=" * 10)
 
-    sig_file.parameters["rewrite_function_mesh"] = False
-    ep_file.parameters["rewrite_function_mesh"] = False
-    sig_P_file.parameters["rewrite_function_mesh"] = False
-    ep_P_file.parameters["rewrite_function_mesh"] = False
+    # Get time difference between two consecutive time steps
+    dt = file_u.attributes(dataset[1])["timestamp"] - file_u.attributes(dataset[0])["timestamp"]
 
-    print_MPI("========== Start post processing ==========")
+    counter = 0
+    for data in dataset:
+        # Read diplacement data and interpolate to P2 space
+        file_d.read(d_p1, data)
+        d_p2.vector()[:] = d_transfer_matrix * d_p1.vector()
 
+        t = file_d.attributes(dataset[counter])["timestamp"]
+        if MPI.rank(MPI.comm_world) == 0:
+            print("=" * 10, f"Calculating Stress & Strain at Timestep: {t}", "=" * 10)
 
-    # NOTE: Instead of using while True, we can use vampy get_datasets function like I did with create_seprate_domain_visualization.py
-    while True:
-        try:
-            f = HDF5File(MPI.comm_world, file_path_d.__str__(), "r")
-            vec_name = "/displacement/vector_%d" % file_counter
-            t = f.attributes(vec_name)["timestamp"]
-            print_MPI("========== Timestep: {} ==========".format(t))
-            f.read(d_viz, vec_name)
-        except Exception as error:
-            print_MPI(error) # An exception occurred
-
-            print_MPI("========== Finished reading solutions ==========")
-            break        
+        # Deformation Gradient for computing cauchy stress
+        deformationF = common.F_(d_p2)
         
-        d_viz.rename("Displacement_test", "d_viz")
+        # Compute Green-Lagrange strain tensor
+        epsilon = common.E(d)
 
-        # Calculate d in P2 based on visualization refined P1
-        d.vector()[:] = dv_trans*d_viz.vector()
-
-        # Deformation Gradient and first Piola-Kirchoff stress (PK1)
-        deformationF = common.F_(d) # calculate deformation gradient from displacement
-        
-        # Cauchy (True) Stress and Infinitesimal Strain (Only accurate for small strains, if other strain desired, check)
-        epsilon = common.eps(d) # Form for Infinitesimal strain (need polar decomposition if we want to calculate logarithmic/Hencky strain)
-
-
+        # TODO: I think intializing those variables here is not necessary or redundant
         # These two loops project the material equations for the fluid and solid domains onto the proper subdomains.
         # This type of projection requires more lines of code but is faster than the built-in "project()" function. 
         # All quantities defined below are linear, bilinear forms and trial and test functions for the tensor and scalar quantities computed
@@ -212,24 +205,38 @@ def compute_stress(visualization_separate_domain_folder, mesh_path, stride):
         ep_P=project_solid(eigStrain11,Scal,dx) # Project onto whole domain
         sig_P=project_solid(eigStress11,Scal,dx)  # Project onto whole domain
 
-        # Name function
-        ep.rename("InfinitesimalStrain", "ep")
-        sig.rename("TrueStress", "sig")
-        ep_P.rename("MaximumPrincipalStrain", "ep_P")
-        sig_P.rename("MaximumPrincipalStress", "sig_P")
+        # Save stress and strain
+        TS.assign(sig)
+        GLS.assign(ep)
+        MPStress.assign(sig_P)
+        MPStrain.assign(ep_P)
 
-        print_MPI("Writing Additional Viz Files for Stresses and Strains!")
+       
+        # Write indices to file
+        for name, xdmf_object in stress_strain.items():
+            variable = stress_strain_dict[name]
+            xdmf_object.write_checkpoint(variable, f"{name}", t, append=True)
 
-        # Write results
-        ep_file.write(ep, t)
-        sig_file.write(sig, t)
-        ep_P_file.write(ep_P, t)
-        sig_P_file.write(sig_P, t)
+def main() -> None:
+    """Main function."""
+    args = parse_arguments()
+    folder_path = args.folder
 
-        # Update file_counter
-        file_counter += stride
+    visualization_separate_domain_folder = args.folder / "Visualization_separate_domain"
+    assert visualization_separate_domain_folder.exists(), f"Visualization_separate_domain folder {visualization_separate_domain_folder} not found."
 
+    if args.mesh_path:
+        mesh_path = Path(args.mesh_path)
+        if MPI.rank(MPI.comm_world) == 0:
+            print("--- Using user-defined mesh \n")
+        assert mesh_path.exists(), f"Mesh file {mesh_path} not found."
+    else:
+        mesh_path = folder_path / "Mesh"
+        if MPI.rank(MPI.comm_world) == 0:
+            print("--- Using mesh from default turrtleFSI Mesh folder \n")
+        assert mesh_path.exists(), f"Mesh file {mesh_path} not found."
 
-if __name__ == '__main__':
-    folder, mesh, _, _, _, dt, stride, save_deg, _, _ = read_command_line()
-    compute_stress(folder,mesh, dt, stride, save_deg)
+    compute_stress(visualization_separate_domain_folder, mesh_path, args.stride)
+
+if __name__ == '__main__':  
+    main()
